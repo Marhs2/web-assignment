c03ccc7000681264ad305d4fbce0fd3a8f5c8761
Marhs2 <wjdehddfl12@gmail.com>
2024-10-03 11:02:07 +0900

    feat: 생성 기능에서 장르 선택 기능 글씨 넣기 성공!

--- script/script.js
+++ script/script.js
@@ -9,18 +9,30 @@ const $Layer = document.querySelector(".layer");
 const $AddBtn = document.querySelector(".add");
 const $BookName = document.querySelector("#book-name");
 const $BookPrice = document.querySelector("#book-price");
-const $BookGen = document.querySelector("#book-gen");
+const $BookGen = document.querySelectorAll(".add-book-gener > div");
+const $BookGenArray = Array.from($BookGen)
+const $Bookstyle = document.querySelector(".book-stlye > div");
 
 function Book_search_funtion() {
   $BookBoxArr.forEach((Books) => {
-    if (Books.childNodes[1].childNodes[1].innerHTML ==  $BookSearch.value) {
+    if (Books.childNodes[1].childNodes[1].innerHTML == $BookSearch.value) {
       Books.style.display = "flex";
-    }else{
+    } else {
       Books.style.display = "none";
-      
     }
   });
 }
+
+function BookStyleSelect() {
+  const $BookGen = $Bookstyle.children;
+  const $BookGenArr = Array.from($Bookstyle)
+  // $BookGenArr.forEach((Gen) => {
+  //   console.log(Gen.innerHTML);
+  // });
+}
+
+//for 이용버전
+
 // function Book_search_funtion() {
 //   for (let i = 0; i < $BookBox.length; i++) {
 //     if ($BookTitle[i].innerHTML == $BookSearch.value) {
@@ -38,29 +50,42 @@ function ShowLayer() {
   $Layer.style.display = "flex";
 }
 
-function AddBook(){
-    $BookContent.innerHTML += `
-          <div class="book">
-        <article>
-          <h2 class="book-title">${$BookName.value}</h2>
-          <a href="#"><img src="./images/Book-img/해리_포터와_마법사의_돌_표지.jpg" alt="hari"></a>
-          <div class="price-about">
-            <p>가격: ${$BookPrice.value}</p>
-            <p>장르: ${$BookGen.value}</p>
-          </div>
-        </article>
-      </div>`;
+function AddBook() {
+  if ($BookName.value == "") {
+    alert("책 이름을 입력하세요");
+  } else {
+    console.log($BookGenArray);
+    $BookGenArray.forEach(Gener => {
+      if(Gener.childNodes[3].checked){
+        $BookContent.innerHTML += `
+        <div class="book">
+      <article>
+        <h2 class="book-title">${$BookName.value}</h2>
+        <a href="#"><img src="./images/Book-img/해리_포터와_마법사의_돌_표지.jpg" alt="hari"></a>
+        <div class="price-about">
+          <p>가격: ${$BookPrice.value}</p>
+          <p>장르: ${Gener.childNodes[1].innerHTML}</p>
+        </div>
+      </article>
+    </div>`;
 
-    $BookGen.value = null;
-    $BookName.value = null;
-    $BookPrice.value = null;
+        $BookGen.value = null;
+        $BookName.value = null;
+        $BookPrice.value = null;
 
-    $Layer.style.display = "none";
-    console.log("작동");
+        $Layer.style.display = "none";
+
+      }
+
+    });
+
+
+
+  }
 }
 
-
-$AddBtn.addEventListener('click',AddBook)
+BookStyleSelect();
+$AddBtn.addEventListener("click", AddBook);
 $AddBookBtn.addEventListener("click", ShowLayer);
 $BookSearch.addEventListener("keypress", (e) => {
   if (e.keyCode == 13) {
